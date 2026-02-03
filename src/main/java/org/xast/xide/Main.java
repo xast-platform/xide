5b02aa923038b2d65e35f0fae653d5b2b00f8420
Koncepto Sociala <sanerhp.mail@gmail.com>
2026-02-03 05:11:37 +0200

    Minimal look

--- src/main/java/org/xast/xide/Main.java
+++ src/main/java/org/xast/xide/Main.java
@@ -1,20 +1,31 @@
 package org.xast.xide;
 
-import com.formdev.flatlaf.FlatDarculaLaf;
-import com.formdev.flatlaf.FlatDarkLaf;
-import com.formdev.flatlaf.intellijthemes.*;
+import com.formdev.flatlaf.extras.FlatSVGIcon;
 import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMTMaterialDarkerIJTheme;
-import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMTMaterialLighterIJTheme;
 
-import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.image.BufferedImage;
 
 import javax.swing.*;
 
-// FlatOneDarkIJTheme
-// FlatMonokaiProIJTheme
-
 public class Main {
+    public static FontLoader fontLoader = new FontLoader();
+    public static Font selawik = fontLoader.loadFont("fonts/selawik/selawk.ttf", 16f);
+    public static Font monospace = fontLoader.loadFont("fonts/jetbrains_mono/JetBrainsMono-Regular.ttf", 16f);
+
     public static void main(String[] args) {
+        UIManager.put("TitlePane.font", Main.selawik);
+        UIManager.put("TitlePane.iconSize", new Dimension(18, 18));
+        UIManager.put("TitlePane.titleMargins", new Insets(8,8,8,8));
+        UIManager.put("TitlePane.buttonSize", new Dimension(44,36));
+        UIManager.put("TabbedPane.tabHeight", 32);
+        UIManager.put("TabbedPane.tabInsets", new Insets(6, 14, 6, 14));
+
         JFrame.setDefaultLookAndFeelDecorated(true);
         JDialog.setDefaultLookAndFeelDecorated(true);
 
@@ -23,38 +34,72 @@ public class Main {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Xide");
             frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            
+            FlatSVGIcon svgIcon = new FlatSVGIcon("icons/logo.svg", 18, 18);
+            BufferedImage img = new BufferedImage(
+                svgIcon.getIconWidth(),
+                svgIcon.getIconHeight(),
+                BufferedImage.TYPE_INT_ARGB
+            );
+            Graphics2D g = img.createGraphics();
+            svgIcon.paintIcon(null, g, 0, 0);
+            g.dispose();
+
+            frame.setIconImage(img);
 
             JMenuBar menuBar = new JMenuBar();
             menuBar.add(new JMenu("File"));
             menuBar.add(new JMenu("Edit"));
             menuBar.add(new JMenu("View"));
-            menuBar.add(new JMenu("Font"));
-            menuBar.add(new JMenu("Options"));
+            menuBar.add(new JMenu("Run"));
+            menuBar.add(new JMenu("Plugins"));
             menuBar.add(new JMenu("Help"));
+            menuBar.setFont(selawik);
             frame.setJMenuBar(menuBar);
             frame.setSize(1280, 960);
             frame.setLocationRelativeTo(null);
 
             JPanel mainPanel = new JPanel();
-            mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+            mainPanel.setLayout(new GridLayout());
 
-            JButton button = new JButton("Click me");
-            button.addActionListener(e -> JOptionPane.showMessageDialog(frame, "Button clicked!"));
-            button.setForeground(Color.LIGHT_GRAY);
-            mainPanel.add(button);
+            JTabbedPane leftDock = new JTabbedPane();
+            leftDock.addTab("Project", new JScrollPane(new JTree()));
+            leftDock.addTab("Files", new JLabel("Files view"));
+            leftDock.setFont(selawik);
 
-            JTextField textField = new JTextField("Enter text here");
-            mainPanel.add(textField);
+            JTabbedPane editorDock = new JTabbedPane();
+            editorDock.addTab("Main.xst", new JScrollPane(new JTextArea("fn main() {}"){{
+                setFont(monospace);
+            }}));
+            editorDock.addTab("Test.xst", new JScrollPane(new JTextArea("// test")));
+            editorDock.setFont(selawik);
 
-            JCheckBox checkBox = new JCheckBox("Accept terms");
-            mainPanel.add(checkBox);
+            JTabbedPane bottomDock = new JTabbedPane();
+            bottomDock.addTab("Console", new JScrollPane(new JTextArea("> ready")));
+            bottomDock.addTab("Build", new JLabel("Build output"));
+            bottomDock.addTab("Logs", new JLabel("Logs"));
+            bottomDock.setFont(selawik);
 
-            JComboBox<String> comboBox = new JComboBox<>(new String[]{"Option 1", "Option 2", "Option 3"});
-            mainPanel.add(comboBox);
+            JSplitPane horizontalSplit = new JSplitPane(
+                    JSplitPane.HORIZONTAL_SPLIT,
+                    leftDock,
+                    editorDock
+            );
+            horizontalSplit.setResizeWeight(0.2);
+            horizontalSplit.setContinuousLayout(true);
+            horizontalSplit.setBorder(null);
 
-            JTextArea textArea = new JTextArea("Multi-line text area", 5, 20);
-            mainPanel.add(new JScrollPane(textArea));
+            JSplitPane verticalSplit = new JSplitPane(
+                    JSplitPane.VERTICAL_SPLIT,
+                    horizontalSplit,
+                    bottomDock
+            );
+            verticalSplit.setResizeWeight(0.75);
+            verticalSplit.setContinuousLayout(true);
+            verticalSplit.setBorder(null);
 
+            mainPanel.add(verticalSplit, BorderLayout.CENTER);
+            
             frame.setContentPane(mainPanel);
             frame.setVisible(true);
         });
